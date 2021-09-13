66be95061329800c46edb93415dacbd9b1ffa356
NeoRot13 <frat-durmaz@web.de>
2021-09-13 07:16:02 +0000

    Sortieren mit ORDER BY / ASC DESC

--- 05_SQL_SELECT/05_2_que_basic.sql
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -10,14 +10,18 @@ Ausgabe beschränken
 # SELECT * FROM stocks.ccc;
 
 -- Begrenzung: LIMIT
+-- Abfragen eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+-- Kombinationen aus mehreren Feldern in neues Feld
+-- hier mit Verknüpfung CONCAT() // String-Fkt.
 SELECT 
     ticker AS "SYM",
     price AS "Kurs in $",
 	c_name AS "Unternehmen",
-	industry AS "Branche"
+	# industry AS "Branche"
+    CONCAT(sector,"|", industry) AS "Operations"
 FROM stocks.ccc
 Limit 10 -- X Zeilen ab 0
-# LIMIT 200,10 -- 10 Zeilen ab 100
+# LIMIT 200,10 -- 10 Zeilen ab 200
 ;
 
 
